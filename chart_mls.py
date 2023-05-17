36bedff176e705fde25310a398a24c6a937e7584
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-17 12:13:25 +0200

    Update chart_mls.py

--- chart_mls.py
+++ chart_mls.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-#import plotly.express as px
+import plotly.express as px
 import plotly.graph_objects as go
 import datetime
 import calendar
@@ -25,7 +25,7 @@ year_choice = st.slider(
         value=2022,
     )
 
-df = df = pd.read_excel('keymls.xlsx')
+df = df = pd.read_excel('https://docs.google.com/spreadsheets/d/1VVjVXYmnDr20f1FjXNYBtglFrPTlJvAu/edit?usp=sharing&ouid=105566543575107705244&rtpof=true&sd=true')
 # -- Apply the year filter given by the user
 res = calendar.monthrange(year_choice, month_choice)[1]
 date = datetime.date(year_choice,month_choice,res)
