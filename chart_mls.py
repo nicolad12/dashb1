bb08026628f3618f4fb8325943fd1ca33d7bd995
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-19 11:48:45 +0200

    Update chart_mls.py

--- chart_mls.py
+++ chart_mls.py
@@ -7,6 +7,62 @@ import calendar
 from st_aggrid import AgGrid, ColumnsAutoSizeMode
 from streamlit_echarts import st_echarts
 
+
+options1 = {
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+    "legend": {
+        "data": ["Direct", "Mail Ad", "Affiliate Ad", "Video Ad", "Search Engine"]
+    },
+    "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+    "xAxis": {"type": "value"},
+    "yAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "series": [
+        {
+            "name": "Direct",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [320, 302, 301, 334, 390, 330, 320],
+        },
+        {
+            "name": "Mail Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+            "name": "Affiliate Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+            "name": "Video Ad",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [150, 212, 201, 154, 190, 330, 410],
+        },
+        {
+            "name": "Search Engine",
+            "type": "bar",
+            "stack": "total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": [820, 832, 901, 934, 1290, 1330, 1320],
+        },
+    ],
+}
+
 options1 = {
     "xAxis": {
         "type": "category",
@@ -17,20 +73,7 @@ options1 = {
         {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
     ],
 }
-st_echarts(options=options1)
-
-with open("USA.json", "r") as f:
-    map = Map(
-        "USA",
-        json.loads(f.read()),
-        {
-            "Alaska": {"left": -131, "top": 25, "width": 15},
-            "Hawaii": {"left": -110, "top": 28, "width": 5},
-            "Puerto Rico": {"left": -76, "top": 26, "width": 2},
-        },
-    )
-options2 = {...}
-st_echarts(options, map=map)
+st_echarts(options=options1, height="500px")
 
  
 #-- Create three columns
