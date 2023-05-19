b8f11e71dc704d68d5dcc04fa4e0d4ebedef499e
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-19 11:43:52 +0200

    Update chart_mls.py

--- chart_mls.py
+++ chart_mls.py
@@ -5,6 +5,19 @@ import plotly.graph_objects as go
 import datetime
 import calendar
 from st_aggrid import AgGrid, ColumnsAutoSizeMode
+from streamlit_echarts import st_echarts
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
+    ],
+}
+st_echarts(options=options)
 
  
 #-- Create three columns
