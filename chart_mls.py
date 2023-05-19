899ff5e5d6a9d4ed2b2d6354fb02135a2de16a35
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-19 11:49:20 +0200

    Update chart_mls.py

--- chart_mls.py
+++ chart_mls.py
@@ -63,16 +63,6 @@ options1 = {
     ],
 }
 
-options1 = {
-    "xAxis": {
-        "type": "category",
-        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    "yAxis": {"type": "value"},
-    "series": [
-        {"data": [820, 932, 901, 934, 1290, 1330, 1320], "type": "line"}
-    ],
-}
 st_echarts(options=options1, height="500px")
 
  
