ed9a77eed8ac6bf044a6c645a65abeb6b7d60171
nicolad12 <133856730+nicolad12@users.noreply.github.com>
2023-05-19 11:53:36 +0200

    Update chart_mls.py

--- chart_mls.py
+++ chart_mls.py
@@ -65,6 +65,38 @@ options1 = {
 
 st_echarts(options=options1, height="500px")
 
+option2 = {
+    "title": {"text": "Project"},
+    "legend": {"data": ["Allocated Budget", "Actual Spending"]},
+    "radar": {
+        "indicator": [
+            {"name": "Sales", "max": 6500},
+            {"name": "Administration", "max": 16000},
+            {"name": "Information Technology", "max": 30000},
+            {"name": "Customer Support", "max": 38000},
+            {"name": "Development", "max": 52000},
+            {"name": "Marketing", "max": 25000},
+        ]
+    },
+    "series": [
+        {
+            "name": "Budget vs spending",
+            "type": "radar",
+            "data": [
+                {
+                    "value": [4200, 3000, 20000, 35000, 50000, 18000],
+                    "name": "Allocated Budget",
+                },
+                {
+                    "value": [5000, 14000, 28000, 26000, 42000, 21000],
+                    "name": "Actual Spending",
+                },
+            ],
+        }
+    ],
+}
+st_echarts(option2, height="500px")
+
  
 #-- Create three columns
 st.title("Project #1 - Key Milestones Chart")
